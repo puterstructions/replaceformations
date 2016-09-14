ad06c7201bc7d55bc467317f1040e517d5d091bd
Daniel McSorley <mcsorley.1@gmail.com>
2016-09-14 14:35:45 -0400

    replace functions

--- main.go
+++ main.go
@@ -28,5 +28,60 @@ func bytesAsJson(b []byte) (interface{}, error) {
 	}
 }
 
+func replaceMap(m map[string]interface{}, resolver func(string) (interface{}, error)) (interface{}, error) {
+	if len(m) == 1 && m["Ref"] != nil {
+		switch refval := m["Ref"].(type) {
+		case string:
+			return m, nil
+		case map[string]interface{}:
+			if len(refval) == 1 && refval["URI"] != nil {
+				switch refurival := refval["URI"].(type) {
+				case string:
+					return resolver(refurival)
+				default:
+					return nil, errors.New("unexpected value for Ref:URI")
+				}
+			} else {
+				replaced, err := replaceMap(refval, resolver)
+				if err != nil {
+					return nil, err
+				}
+				return map[string]interface{} { "Ref": replaced }, nil
+			}
+		default:
+			return nil, errors.New("unknown reftype")
+		}
+	}
+	returnable := make(map[string]interface{})
+	for k, v := range m {
+		val, err := replace(v, resolver)
+		if err != nil {
+			return nil, err
+		}
+		returnable[k] = val
+	}
+	return returnable, nil
+}
+
+func replace(i interface{}, resolver func(string) (interface{}, error)) (interface{}, error) {
+	switch value := i.(type) {
+	case string:
+		return value, nil
+	case map[string]interface{}:
+		return replaceMap(value, resolver)
+	default:
+		return nil, nil
+	}
+}
+
+func replaceBytes(b []byte, resolver func(string) (interface{}, error)) (interface{}, error) {
+	i, err := bytesAsJson(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return replace(i, resolver)
+}
+
 func main() {
 }
--- main_test.go
+++ main_test.go
@@ -31,3 +31,169 @@ func TestBytesAsJsonMap(t *testing.T) {
 		t.Errorf("'%v' is not uiopas", v)
 	}
 }
+
+func equivalentString(value string, expected interface{}, t *testing.T) {
+	switch v2 := expected.(type) {
+	case string:
+		if value != v2 {
+			t.Errorf("%s is not an equivalent string to %s", value, expected)
+		}
+	default:
+		t.Errorf("%v is not a string", expected)
+	}
+}
+
+func equivalentMap(value map[string]interface{}, expected interface{}, t *testing.T) {
+	switch v2 := expected.(type) {
+	case map[string]interface{}:
+		if len(value) != len(v2) {
+			t.Errorf("len(%v) != len(%v)", value, v2)
+		}
+		for k, v := range value {
+			equivalentJson(v, v2[k], t)
+		}
+	default:
+		t.Errorf("%v is not a map", expected)
+	}
+}
+
+func equivalentJson(value interface{}, expected interface{}, t *testing.T) {
+	switch v := value.(type) {
+	case string:
+		equivalentString(v, expected, t)
+	case map[string]interface{}:
+		equivalentMap(v, expected, t)
+	default:
+		t.Errorf("%v is not equivalent to %v", value, expected)
+	}
+}
+
+func asJson(raw []byte, t *testing.T) interface{} {
+	v, err := bytesAsJson(raw)
+	if err != nil {
+		t.Error(err)
+	}
+	return v
+}
+
+func TestEquivalent(t *testing.T) {
+	equivalentJson(
+		asJson([]byte(`"asdf"`), t),
+		asJson([]byte(`"asdf"`), t),
+		t,
+	)
+	equivalentJson(
+		asJson([]byte(`{"foo":"bar"}`), t),
+		asJson([]byte(`{"foo":"bar"}`), t),
+		t,
+	)
+}
+
+func resolver(t *testing.T) func(string) (interface{}, error) {
+	return func(name string) (interface{}, error) {
+		return asJson([]byte(`{"a":"thing"}`), t), nil
+	}
+}
+
+func TestIdentityReplaceString(t *testing.T) {
+	replaced, err := replace(asJson([]byte(`"zxcvb"`), t), resolver(t))
+	if err != nil {
+		t.Error(err)
+	}
+
+	equivalentJson(
+		replaced,
+		asJson([]byte(`"zxcvb"`), t),
+		t,
+	)
+}
+
+func TestIdentityReplaceAMap(t *testing.T) {
+	template := `{"asdf":"qwerty"}`
+	replaced, err := replace(asJson([]byte(template), t), resolver(t))
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := asJson([]byte(template), t)
+	equivalentJson(
+		replaced,
+		expected,
+		t,
+	)
+}
+
+func TestReplaceMapIdentity(t *testing.T) {
+	template := map[string]interface{} { "asdf": "qwerty" }
+	replaced, err := replaceMap(template, resolver(t))
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := asJson([]byte(`{"asdf":"qwerty"}`), t)
+	equivalentJson(
+		replaced,
+		expected,
+		t,
+	)
+}
+
+func TestReplaceMapRefIdentity(t *testing.T) {
+	template := map[string]interface{} { "Ref": "foo" }
+	replaced, err := replaceMap(template, resolver(t))
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := asJson([]byte(`{"Ref":"foo"}`), t)
+	equivalentJson(
+		replaced,
+		expected,
+		t,
+	)
+}
+
+func TestReplaceMapRefURI(t *testing.T) {
+	template := map[string]interface{} { "Ref": map[string]interface{} { "URI": "somecomponent" } }
+	replaced, err := replaceMap(template, resolver(t))
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := asJson([]byte(`{"a":"thing"}`), t)
+	equivalentJson(
+		replaced,
+		expected,
+		t,
+	)
+}
+
+func TestIdentityRef(t *testing.T) {
+	template := `{"asdf":{"Ref": "NamedValue"}}`
+	replaced, err := replace(asJson([]byte(template), t), resolver(t))
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := asJson([]byte(template), t)
+	equivalentJson(
+		replaced,
+		expected,
+		t,
+	)
+}
+
+func TestReplace(t *testing.T) {
+	template := `{"asdf":{"Ref": {"URI":"componentName"}}}`
+	replaced, err := replace(asJson([]byte(template), t), resolver(t))
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := asJson([]byte(`{"asdf":{"a":"thing"}}`), t)
+	equivalentJson(
+		replaced,
+		expected,
+		t,
+	)
+}
